32ac7f78ed728473aa7796e6a8fc6edaa5cb83c2
systemnet <systemnet@yandex.ru>
2020-12-20 01:28:15 +0200

    stream load to s3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,25 +1,11 @@
-import {
-  Controller,
-  Get,
-  Param,
-  Post,
-  Req,
-  Res,
-  UploadedFile,
-} from '@nestjs/common';
+import { Controller, Get, Param, Post, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-import { UseInterceptors } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { ConfigService } from '@nestjs/config';
 import { ImageS3Service } from './image-s3/image-s3.service';
-import * as fs from 'fs';
-import * as sharp from 'sharp';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private readonly configService: ConfigService,
     private readonly imageS3Service: ImageS3Service,
   ) {}
 
@@ -29,37 +15,16 @@ export class AppController {
   }
 
   @Post(':filename')
-  // @UseInterceptors(FileInterceptor('image'))
-  async imageUpload(
-    // @UploadedFile() image,
-    @Res() res,
-    @Param() params,
-    @Req() req,
-  ) {
-    // const { filename } = params;
-    // this.imageS3Service
-    //   .uploadImage(image.buffer, filename)
-    //   .then(() => {
-    //     res.send('ok');
-    //   })
-    //   .catch(() => res.status(500).send('Failed to upload image into AWS S3!'));
-    // console.log('Image: ', image);
-    // console.log('Req: ', req.buffer);
+  async imageUpload(@Param() params, @Res() res, @Req() req) {
+    const { filename } = params;
 
-    // const length = 0;
-    // const chunks = [];
-    const fileStream2048 = fs.createWriteStream('uploads/logo-2048.jpg');
-    const fileStream1024 = fs.createWriteStream('uploads/logo-1024.jpg');
-    const fileStream300 = fs.createWriteStream('uploads/logo-300.jpg');
-
-    const resize2048 = sharp().resize({ width: 2048 });
-    const resize1024 = sharp().resize({ width: 1024 });
-    const resize300 = sharp().resize({ width: 300 });
-
-    req.pipe(resize2048).pipe(fileStream2048);
-    req.pipe(resize1024).pipe(fileStream1024);
-    req.pipe(resize300).pipe(fileStream300);
-
-    res.send('ok');
+    this.imageS3Service
+      .uploadImage(req, filename)
+      .then((fname) => {
+        res.send(fname);
+      })
+      .catch((e) => {
+        res.status(500).send(e.toString());
+      });
   }
 }
--- src/image-s3/image-s3.service.ts
+++ src/image-s3/image-s3.service.ts
@@ -1,26 +1,62 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { S3 } from 'aws-sdk';
+import * as sharp from 'sharp';
+import * as stream from 'stream';
 
 @Injectable()
 export class ImageS3Service {
   constructor(private readonly configService: ConfigService) {}
 
-  async uploadImage(dataBuffer: Buffer, filename: string) {
-    const awsS3 = new S3();
-    awsS3.config.update({
-      accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+  async uploadImage(request, filename: string) {
+    return new Promise(async (resolve, reject) => {
+      const contype = request.headers['content-type']; // getting Content-Type from header
+      const conlength = parseInt(request.headers['content-length']); // getting Content-Length from header
+      if (!contype) return reject('Content-Type not found!');
+      if (conlength > this.configService.get('MAX_UPLOAD_IMAGE_SIZE'))
+        return reject(
+          `File size too large! Allowed: ${this.configService.get(
+            'MAX_UPLOAD_IMAGE_SIZE',
+          )} bytes`,
+        );
+
+      const contype_arr = contype.split('/');
+      const type = contype_arr[0];
+      const ext = contype_arr[1];
+      if (type !== 'image') {
+        return reject('Must be only image!');
+      }
+
+      const allow_formats = this.configService.get('ALLOW_IMAGE_FORMAT');
+      if (allow_formats !== undefined && allow_formats?.indexOf(ext) < 0) {
+        return reject('Content-Type is not allowed!');
+      }
+
+      const s3 = new S3();
+      const s3Stream = new stream.PassThrough();
+
+      s3.upload({
+        Body: s3Stream,
+        Key: `${filename}-300.${ext}`,
+        Bucket: this.configService.get('AWS_PUBLIC_BUCKET_NAME'),
+      }).send((err, data) => {
+        if (err) {
+          s3Stream.destroy(err);
+          return reject(err);
+        } else {
+          console.log(`File uploaded and available at ${data.Location}`);
+          s3Stream.destroy();
+          return resolve(data.Location);
+        }
+      });
+
+      const resize300 = sharp()
+        .resize(300)
+        .on('error', (err) => {
+          return reject(err);
+        });
+
+      request.pipe(resize300).pipe(s3Stream);
     });
-    return true;
-    // const uploadResult = await awsS3
-    //   .upload({
-    //     Bucket: this.configService.get('AWS_PUBLIC_BUCKET_NAME'),
-    //     Body: dataBuffer,
-    //     Key: filename,
-    //   })
-    //   .promise();
-    //
-    // return uploadResult;
   }
 }
